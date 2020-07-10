1f63b9b2909dca50e5518b2868e5bbd7ccef5b07
Drew McMillan <drew.mcmillan@thetrainline.com>
2020-07-11 00:16:31 +0100

    update

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const getTable = (tracks, artists) => {
 const getImage1 = (thing) =>
   `<img src="${
     (thing.album ? thing.album.images : thing.images).pop().url
-  }" alt="Photo of ${thing.name}" width="40px" align="left" />`;
+  }" alt="Photo of ${thing.name}" height="50px" />`;
 
 const template = (artists, tracks) => {
   const lines = [
